4f4733f7ac6e42cbdd1566a574cfce0f186c9cc7
404 <1347788561@qq.com>
2020-11-17 21:33:21 +0800

    [feat] userinfo & bodyinfo

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,6 +21,9 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
+      // wx.redirectTo({
+      //   url: '/pages/home/home',
+      // })
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
@@ -29,6 +32,9 @@ Page({
           userInfo: res.userInfo,
           hasUserInfo: true
         })
+        // wx.redirectTo({
+        //   url: '/pages/home/home',
+        // })
       }
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
@@ -39,6 +45,9 @@ Page({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
+          // wx.redirectTo({
+          //   url: '/pages/home/home',
+          // })
         }
       })
     }
@@ -48,7 +57,10 @@ Page({
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      hasUserInfo: true,
     })
-  }
+    // wx.redirectTo({
+    //   url: '/pages/home/home',
+    // })
+  },
 })
